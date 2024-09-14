cb00722c60bbe84992d6ee0cb17eae1d4d94f922
Dramaturg1 <widowmakerNoOver@outlook.com>
2024-09-14 12:26:56 +0300

    fixeed delegate array

--- lab3/Main.Designer.cs
+++ lab3/Main.Designer.cs
@@ -28,65 +28,55 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
             this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Группа №1 (BubbleSort, InsertionSort, SelectionSort, ShakerSort, GnomeSort)",
+            "Группа №2 (BitonicSort, ShellSort, TreeSort)",
+            "Группа №3 (CombSort, HeapSort, QuickSort, MergeSort, CountingSort, BucketSort, Ra" +
+                "diaxSort)"});
+            this.comboBox1.Location = new System.Drawing.Point(93, 130);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(593, 24);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "Массивы случайных чисел",
+            "Массивы с отсортированными подмассивами",
+            "Отсортированные массивы с перестановками",
+            "Массивы с повторениями; массивы, остортированные прямо и обратно"});
+            this.comboBox2.Location = new System.Drawing.Point(93, 273);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(593, 24);
+            this.comboBox2.TabIndex = 1;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(91, 50);
+            this.button1.Location = new System.Drawing.Point(240, 406);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(338, 93);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Сортировка массивов случайных чисел по модулям 10, 1000, 10^5, 10^7, 10^9\r\n";
+            this.button1.Size = new System.Drawing.Size(277, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(91, 176);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(338, 93);
-            this.button2.TabIndex = 2;
-            this.button2.Text = "Сортировка массивов, разбитых на несколько отсортированных подмассивов разного ра" +
-    "змера";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
-            // 
-            // button3
-            // 
-            this.button3.Location = new System.Drawing.Point(91, 301);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(338, 93);
-            this.button3.TabIndex = 3;
-            this.button3.Text = "Сортировка изначально отсортированных массивов случайных чисел с некоторым числом" +
-    "\r\nперестановок двух случайных элементов";
-            this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
-            // 
-            // button4
-            // 
-            this.button4.Location = new System.Drawing.Point(91, 433);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(338, 93);
-            this.button4.TabIndex = 4;
-            this.button4.Text = "Сортировка полностью отсортированных массивов, с\r\nнесколькими заменёнными элемент" +
-    "ами,с большим количеством\r\nповторений одного элемента";
-            this.button4.UseVisualStyleBackColor = true;
-            this.button4.Click += new System.EventHandler(this.button4_Click);
-            // 
-            // Menu
+            // Main
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(520, 570);
-            this.Controls.Add(this.button4);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
+            this.ClientSize = new System.Drawing.Size(802, 570);
             this.Controls.Add(this.button1);
-            this.Name = "Menu";
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Main";
             this.Text = "Form1";
             this.ResumeLayout(false);
 
@@ -94,10 +84,9 @@
 
         #endregion
 
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button4;
     }
 }
 
--- lab3/Main.cs
+++ lab3/Main.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -17,28 +18,62 @@ namespace lab3
             InitializeComponent();
         }
 
-        private void button1_Click(object sender, System.EventArgs e)
-        {
-            Form basicArray = new BasicSort();
-            basicArray.Show();
-        }
 
-        private void button2_Click(object sender, System.EventArgs e)
-        {
-            Form subarrays = new Subarrays();
-            subarrays.Show();
-        }
+        public delegate void SortDelegate(int[] array);
 
-        private void button3_Click(object sender, System.EventArgs e)
-        {
-            Form sortedArrays = new SortedArray();
-            sortedArrays.Show();
-        }
+        List<SortDelegate> sortAlgs = new List<SortDelegate>
+            {
+                sorting.SortingAlgorithms.BubbleSort, //0
+                sorting.SortingAlgorithms.InsertionSort, //1
+                sorting.SortingAlgorithms.SelectionSort, //2
+                sorting.SortingAlgorithms.ShakerSort, //3
+                sorting.SortingAlgorithms.GnomeSort, //4
+                sorting.SortingAlgorithms.BitonicSort, //5
+                sorting.SortingAlgorithms.ShellSort, //6
+                sorting.SortingAlgorithms.TreeSort, //7
+                sorting.SortingAlgorithms.CombSort, //8
+                sorting.SortingAlgorithms.HeapSort, //9
+                sorting.SortingAlgorithms.QuickSort, //10
+                sorting.SortingAlgorithms.MergeSort, //11
+                sorting.SortingAlgorithms.CountingSort, //12
+                sorting.SortingAlgorithms.BucketSort, //13
+                sorting.SortingAlgorithms.RadiaxSort, //14
+            };
 
-        private void button4_Click(object sender, System.EventArgs e)
+        private long[] RandMod10()
         {
-            Form duplicates = new Duplicates();
-            duplicates.Show();
+            Stopwatch timer = new Stopwatch();
+            Random rand = new Random();
+            long[] elapsedMS = new long[15];
+            foreach (long i in elapsedMS)
+            {
+                elapsedMS[i] = 0;
+            };
+
+
+
+            for (int i = 0; i < 15; i++)
+            {
+                for (int j = 0; j < 20; j++)
+                {
+                    int[] array = new int[100000];
+                    for (int k = 0; k < array.Length; k++)
+                    {
+                        array[k] = rand.Next(-9, 9);
+                    }
+                    timer.Start();
+                    sortAlgs[i](array);
+                    timer.Stop();
+                    elapsedMS[i] += timer.ElapsedMilliseconds;
+                    timer.Reset();
+                }
+
+            }
+            foreach (int i in elapsedMS)
+            {
+                elapsedMS[i] /= 20;
+            }
+            return elapsedMS;
         }
     }
 }
