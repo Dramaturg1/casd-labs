0c7f767922ed57cb0a8b0fc73ee88e8d5ad2621c
Dramaturg1 <widowmakerNoOver@outlook.com>
2024-09-18 10:31:16 +0300

    fixed the programm

--- lab5/Program.cs
+++ lab5/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using System.Runtime.CompilerServices;
 
 namespace lab5
 {
@@ -10,6 +12,50 @@ namespace lab5
     {
         static void Main(string[] args)
         {
+            List<string> tagValue = new List<string>();
+            Stack<char> tags = new Stack<char>();
+            string path = "C:/Users/Sofok/source/repos/lab5/lab5/bin/Debug/input.txt";
+            using (StreamReader file = new StreamReader(path))
+            {
+                string temp;
+                while ((temp = file.ReadLine()) != null)
+                {
+                    Console.WriteLine(temp);
+                    string value = "";
+                    for (int i = 0; i < temp.Length; i++)
+                    {
+                        if (temp[i] == '<')
+                        {
+                            tags.Push(temp[i]);
+                        }
+                        else if (temp[i] == '>')
+                        {
+                            tags.Pop();
+                            if (!string.IsNullOrEmpty(value))
+                            {
+                                tagValue.Add(value);
+                                value = "";
+                            }
+                        }
+                        else if (temp[i] != '/' && temp[i] != ' ')
+                        {
+                            value += temp[i];
+                        }
+                    }
+                }
+            }
+            HashSet<string> strings = new HashSet<string>();
+            for (int i = 0; i < tagValue.Count; i++)
+            {
+                tagValue[i] = tagValue[i].ToLower();
+                strings.Add(tagValue[i]);
+            }
+            foreach (string s in strings)
+            {
+                Console.Write(s + " ");
+            }
+            
+            Console.ReadLine();
         }
     }
 }
