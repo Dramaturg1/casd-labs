c60cc6a0c6dbc0cdb44d29a7556221e08a863721
Dramaturg1 <widowmakerNoOver@outlook.com>
2024-09-19 11:47:10 +0300

    added methods prototypes

--- lab6/ClassMyVector.cs
+++ lab6/ClassMyVector.cs
@@ -1,12 +1,156 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace lab6
 {
-    internal class Class1
+    public class MyVector<T>
     {
+        private T[] elementData;
+        private int elementCount;
+        private int capacityIncrement;
+
+        public MyVector(int initialCapacity, int capacityIncrement)
+        {
+
+        }
+
+        public MyVector(int initialCapacity)
+        {
+        }
+
+        public MyVector()
+        {
+
+        }
+
+        public MyVector(T[] a)
+        {
+
+        }
+
+        public void Add(T e)
+        {
+
+        }
+
+        public void AddAll(T[] a)
+        {
+
+        }
+
+        public void Clear()
+        {
+        }
+
+        public bool Contains(object o)
+        {
+
+        }
+
+        public bool ContainsAll(T[] a)
+        {
+
+        }
+
+        public bool IsEmpty()
+        {
+
+        }
+
+        public void Remove(object o)
+        {
+
+        }
+
+        public void RemoveAll(T[] a)
+        {
+        }
+
+        public void RetainAll(T[] a)
+        {
+
+        }
+
+        public int Size()
+        {
+
+        }
+
+        public T[] ToArray()
+        {
+
+        }
+
+        public T[] ToArray(T[] a)
+        {
+
+        }
+
+        public void Add(int index, T e)
+        {
+
+        }
+
+        public void AddAll(int index, T[] a)
+        {
+
+        }
+
+        public T Get(int index)
+        {
+
+        }
+
+        public int indexOf(object o)
+        {
+
+        }
+
+        public int LastIndexOf(object o)
+        {
+
+        }
+
+        public void Remove(int index) 
+        {
+
+        }
+
+        public void Set(int index, T e)
+        {
+
+        }
+
+        public MyVector<T> SubList(int fromIndex, int toIndex)
+        {
+
+        }
+
+        public object FirstElement()
+        {
+
+        }
+
+        public object LastElement()
+        {
+
+        }
+
+        public void RemoveElementAt(int pos)
+        {
+
+        }
+
+        public void RemoveRange(int begin, int end)
+        {
+
+        }
+
+
     }
 }
