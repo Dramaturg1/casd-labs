e55a27a52209cb4606a717191089127331a77d80
Dramaturg1 <widowmakerNoOver@outlook.com>
2024-12-26 03:06:53 +0300

    Add project files.

---
+++ lab12/MyRequest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab12
+{
+    internal class MyRequest : IComparable<MyRequest>
+    {
+        private int priority;
+        private int number;
+        private int step;
+        private Stopwatch stopwatch;
+
+        public int Priority => priority;
+        public int Number => number;
+        public int Step => step;
+        public Stopwatch Stopwatch => stopwatch;
+
+        public MyRequest(int priority, int number, int step)
+        {
+            this.priority = priority;
+            this.number = number;
+            this.step = step;
+            this.stopwatch = Stopwatch.StartNew();
+        }
+        public int CompareTo(MyRequest other)
+        {
+            if (other == null)
+                return 1;
+            return this.priority.CompareTo(other.priority);
+        }
+    }
+}
---
+++ lab12/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using lab10;
+using lab11;
+
+namespace lab12
+{
+    internal class Program
+    {
+        private static int numberRequest = 1;
+        private static string filePath = "log.txt";
+        private static StreamWriter writer = new StreamWriter(filePath);
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.OutputEncoding = Encoding.GetEncoding(1251);
+                int n = Convert.ToInt32(Console.ReadLine());
+                MyPriorityQueue<MyRequest> queue = new MyPriorityQueue<MyRequest>();
+                for (int i = 1; i < n + 1; i++)
+                {
+                    Random rnd = new Random();
+                    int countRequests = rnd.Next(1, 10);
+                    for (int j = 0; j < countRequests; j++)
+                    {
+                        GenerateRequest(queue, i);
+                    }
+                    MyRequest maxRequest = queue.Element();
+                    queue.Remove(maxRequest);
+                    writer.WriteLine("REMOVE " + maxRequest.Number + " " + maxRequest.Priority + " " + maxRequest.Step);
+                }
+                System.TimeSpan time = new TimeSpan();
+                int pr = 0, st = 0, num = 0;
+                while (!queue.IsEmpty())
+                {
+                    MyRequest req = queue.Element();
+                    time = req.Stopwatch.Elapsed;
+                    pr = req.Priority;
+                    st = req.Step;
+                    num = req.Number;
+                    queue.Remove(req);
+                    writer.WriteLine("REMOVE " + req.Number + " " + req.Priority + " " + req.Step);
+                }
+                queue = null;
+                Console.WriteLine(num + " " + pr + " " + st + " " + time);
+                writer.Close();
+                Console.WriteLine("Data has been updated in " + Directory.GetCurrentDirectory() + "\\log.txt");
+                Console.ReadLine();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+                Console.ReadLine();
+            }
+        }
+        private static void GenerateRequest(MyPriorityQueue<MyRequest> queue, int step)
+        {
+
+            Random rnd = new Random();
+            int countRequests = rnd.Next(1, 10);
+
+            for (int i = 0; i <= countRequests; i++)
+            {
+                int priority = rnd.Next(1, 5);
+                MyRequest request = new MyRequest(priority, numberRequest, step);
+                numberRequest++;
+                queue.Add(request);
+                writer.WriteLine("ADD " + request.Number + " " + request.Priority + " " + request.Step);
+            }
+        }
+    }
+}
